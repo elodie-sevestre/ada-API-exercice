6ee24c43d7fa7900c77f025c3074128fe29794cc
Elodie S <elodie.sevestre86@gmail.com>
2026-03-30 15:38:14 +0200

    suite renderOpenbMeteo ()

--- src/calling/open-meteo.js
+++ src/calling/open-meteo.js
@@ -18,3 +18,50 @@ export async function callOpenMeteoAPI(lat, lon) {
     console.error(err);
   }
 }
+
+// ========================================= VARIABLES ===========================================
+
+const cities = {
+  Nantes: {
+    lat: 47.2184,
+    lon: -1.5536,
+  },
+  Paris: {
+    lat: 48.8566,
+    lon: 2.3522,
+  },
+  Rennes: {
+    lat: 48.1173,
+    lon: -1.6778,
+  },
+  Caen: {
+    lat: 49.1829,
+    lon: -0.3707,
+  },
+  Marseille: {
+    lat: 43.2965,
+    lon: 5.3698,
+  },
+};
+
+// =========================================== DOM ===============================================
+
+Object.keys(cities).forEach((city) => {
+  document
+    .querySelector(`#${city.toLowerCase()}`)
+    .addEventListener("click", () => {
+      renderOpenMeteo(city);
+    });
+});
+
+// ============================================= API =============================================
+
+/**
+ * Lance la requête vers l'API et remplace les données une fois obtenue.
+ */
+export let renderOpenMeteo = async (city) => {
+  // TODO
+  const data = await callOpenMeteoAPI(cities[city].lat, cities[city].lon);
+  const currentWeather = document.querySelector(".current-weather");
+  currentWeather.textContent = data.current.temperature_2m;
+};
--- src/main.js
+++ src/main.js
@@ -1,37 +1,10 @@
-import "./style.css";
-import { callOpenMeteoAPI } from "./calling/open-meteo";
-/**
- * Lance la requête vers l'API et remplace les données une fois obtenue.
- */
-let renderOpenMeteo = async () => {
-  // TODO
-  const data = await callOpenMeteoAPI(cities.Nantes.lat, cities.Nantes.lon);
-};
+// ========================================== IMPORTS ============================================
 
-const cities = {
-  Nantes: {
-    lat: 47.2184,
-    lon: -1.5536,
-  },
-  Paris: {
-    lat: 48.8566,
-    lon: 2.3522,
-  },
-  Rennes: {
-    lat: 48.1173,
-    lon: -1.6778,
-  },
-  Caen: {
-    lat: 49.1829,
-    lon: -0.3707,
-  },
-  Marseille: {
-    lat: 43.2965,
-    lon: 5.3698,
-  },
-};
+import "./style.css";
+import { renderOpenMeteo } from "./calling/open-meteo";
 
 // =====================================================================
 // - - - - - - - - - - - E X E C U T I O N - - - - - - - - - - - - - - -
 // =====================================================================
-renderOpenMeteo();
+
+renderOpenMeteo(city);
