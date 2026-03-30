37906d5b7d717e23d698bea57271d633797967aa
Elodie S <elodie.sevestre86@gmail.com>
2026-03-30 11:57:53 +0200

    init main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,33 @@
-import './style.css'
-
+import "./style.css";
 /**
  * Lance la requête vers l'API et remplace les données une fois obtenue.
  */
 let renderOpenMeteo = async () => {
-    // TODO
-}
+  // TODO
+};
+
+const cities = {
+  Nantes: {
+    lat: 47.2184,
+    lon: -1.5536,
+  },
+  Paris: {
+    lat: 48.8566,
+    lon: 2.3522,
+  },
+  Rennes: {
+    lat: 48.1173,
+    lon: -1.6778,
+  },
+  Caen: {
+    lat: 49.1829,
+    lon: -0.3707,
+  },
+  Marseille: {
+    lat: 43.2965,
+    lon: 5.3698,
+  },
+};
 
 // =====================================================================
 // - - - - - - - - - - - E X E C U T I O N - - - - - - - - - - - - - - -
