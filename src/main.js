83420a49a5f52a74fb9b86bf2fb996720a328c48
Elodie S <elodie.sevestre86@gmail.com>
2026-03-31 12:24:42 +0200

    done API exercice

--- src/main.js
+++ src/main.js
@@ -28,37 +28,36 @@ const cities = {
   },
 };
 
-const weatherIcons = {
-  0: "☀️",
-  1: "🌤️",
-  2: "⛅",
-  3: "☁️",
-  45: "🌫️",
-  48: "🌫️",
-  51: "🌦️",
-  53: "🌦️",
-  55: "🌧️",
-  56: "🌧️",
-  57: "🌧️",
-  61: "🌧️",
-  63: "🌧️",
-  65: "🌧️",
-  66: "🌨️",
-  67: "🌨️",
-  71: "❄️",
-  73: "❄️",
-  75: "❄️",
-  77: "🌨️",
-  80: "🌦️",
-  81: "🌧️",
-  82: "⛈️",
-  85: "🌨️",
-  86: "🌨️",
-  95: "⛈️",
-  96: "⛈️",
-  99: "⛈️",
+const weatherData = {
+  0: { icon: "☀️", description: "Ciel dégagé" },
+  1: { icon: "🌤️", description: "Principalement dégagé" },
+  2: { icon: "⛅", description: "Partiellement nuageux" },
+  3: { icon: "☁️", description: "Couvert" },
+  45: { icon: "🌫️", description: "Brouillard" },
+  48: { icon: "🌫️", description: "Brouillard givrant" },
+  51: { icon: "🌦️", description: "Bruine légère" },
+  53: { icon: "🌦️", description: "Bruine modérée" },
+  55: { icon: "🌧️", description: "Bruine dense" },
+  56: { icon: "🌧️", description: "Bruine verglaçante légère" },
+  57: { icon: "🌧️", description: "Bruine verglaçante dense" },
+  61: { icon: "🌧️", description: "Pluie légère" },
+  63: { icon: "🌧️", description: "Pluie modérée" },
+  65: { icon: "🌧️", description: "Pluie forte" },
+  66: { icon: "🌨️", description: "Pluie verglaçante légère" },
+  67: { icon: "🌨️", description: "Pluie verglaçante forte" },
+  71: { icon: "❄️", description: "Neige légère" },
+  73: { icon: "❄️", description: "Neige modérée" },
+  75: { icon: "❄️", description: "Neige forte" },
+  77: { icon: "🌨️", description: "Grains de neige" },
+  80: { icon: "🌦️", description: "Averses légères" },
+  81: { icon: "🌧️", description: "Averses modérées" },
+  82: { icon: "⛈️", description: "Averses violentes" },
+  85: { icon: "🌨️", description: "Averses de neige légères" },
+  86: { icon: "🌨️", description: "Averses de neige fortes" },
+  95: { icon: "⛈️", description: "Orage" },
+  96: { icon: "⛈️", description: "Orage avec grêle légère" },
+  99: { icon: "⛈️", description: "Orage avec grêle forte" },
 };
-
 // =========================================== API ===============================================
 
 /**
@@ -68,11 +67,13 @@ const renderOpenMeteo = async (city) => {
   // TODO
   const data = await callOpenMeteoAPI(cities[city].lat, cities[city].lon);
   const currentWeather = document.querySelector(".current-weather");
-  currentWeather.textContent = `${weatherIcons[data.current.weather_code]} ${data.current.temperature_2m}°C`;
+  currentWeather.textContent = `${weatherData[data.current.weather_code].icon} 
+  ${weatherData[data.current.weather_code].description}  
+  ${data.current.temperature_2m}°C`;
   const listItems = document.querySelectorAll("li");
   data.daily.time.slice(0, 5).forEach((day, index) => {
     listItems[index].textContent =
-      `${new Date(day).toLocaleDateString("fr-FR", { weekday: "short", day: "numeric", month: "short", year: "numeric" })} : ${weatherIcons[data.daily.weather_code[index]]} ${data.daily.temperature_2m_min[index]} / ${data.daily.temperature_2m_max[index]}°C`;
+      `${new Date(day).toLocaleDateString("fr-FR", { weekday: "short", day: "numeric", month: "short", year: "numeric" })} : ${weatherData[data.daily.weather_code[index]].icon}  ${weatherData[data.daily.weather_code[index]].description} ${data.daily.temperature_2m_min[index]} / ${data.daily.temperature_2m_max[index]}°C`;
   });
 };
 
