b1ddbd8f93a5c8ac417b297c9caef99c05d3d6e9
Elodie S <elodie.sevestre86@gmail.com>
2026-03-31 11:39:05 +0200

    modif main.js

--- src/calling/open-meteo.js
+++ src/calling/open-meteo.js
@@ -9,7 +9,7 @@ export async function callOpenMeteoAPI(lat, lon) {
   try {
     // TODO `https://api.open-meteo.com/`
     const response = await fetch(
-      `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,weather_code`,
+      `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min`,
     ); // données RAW / consulter la doc pour avoir l'URL
     const data = await response.json(); // transforme RAW en JSON
     console.log(data);
--- src/main.js
+++ src/main.js
@@ -28,6 +28,37 @@ const cities = {
   },
 };
 
+const weatherDescriptions = {
+  0: "Ciel dégagé",
+  1: "Principalement dégagé",
+  2: "Partiellement nuageux",
+  3: "Couvert",
+  45: "Brouillard",
+  48: "Brouillard givrant",
+  51: "Bruine légère",
+  53: "Bruine modérée",
+  55: "Bruine dense",
+  56: "Bruine verglaçante légère",
+  57: "Bruine verglaçante dense",
+  61: "Pluie légère",
+  63: "Pluie modérée",
+  65: "Pluie forte",
+  66: "Pluie verglaçante légère",
+  67: "Pluie verglaçante forte",
+  71: "Neige légère",
+  73: "Neige modérée",
+  75: "Neige forte",
+  77: "Grains de neige",
+  80: "Averses légères",
+  81: "Averses modérées",
+  82: "Averses violentes",
+  85: "Averses de neige légères",
+  86: "Averses de neige fortes",
+  95: "Orage",
+  96: "Orage avec grêle légère",
+  99: "Orage avec grêle forte",
+};
+
 // =========================================== API ===============================================
 
 /**
@@ -37,7 +68,7 @@ const renderOpenMeteo = async (city) => {
   // TODO
   const data = await callOpenMeteoAPI(cities[city].lat, cities[city].lon);
   const currentWeather = document.querySelector(".current-weather");
-  currentWeather.textContent = data.current.temperature_2m;
+  currentWeather.textContent = `${weatherDescriptions[data.current.weather_code]} ${data.current.temperature_2m}°C`;
 };
 
 // =========================================== DOM ===============================================
@@ -54,4 +85,4 @@ Object.keys(cities).forEach((city) => {
 // - - - - - - - - - - - E X E C U T I O N - - - - - - - - - - - - - - -
 // =====================================================================
 
-renderOpenMeteo(city);
+renderOpenMeteo("Nantes");
