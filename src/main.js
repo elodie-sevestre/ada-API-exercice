521847de2356dfb622a2e766b6f77cee44821ad3
Elodie S <elodie.sevestre86@gmail.com>
2026-03-31 11:57:25 +0200

    daily 5 days

--- src/main.js
+++ src/main.js
@@ -28,35 +28,35 @@ const cities = {
   },
 };
 
-const weatherDescriptions = {
-  0: "Ciel dégagé",
-  1: "Principalement dégagé",
-  2: "Partiellement nuageux",
-  3: "Couvert",
-  45: "Brouillard",
-  48: "Brouillard givrant",
-  51: "Bruine légère",
-  53: "Bruine modérée",
-  55: "Bruine dense",
-  56: "Bruine verglaçante légère",
-  57: "Bruine verglaçante dense",
-  61: "Pluie légère",
-  63: "Pluie modérée",
-  65: "Pluie forte",
-  66: "Pluie verglaçante légère",
-  67: "Pluie verglaçante forte",
-  71: "Neige légère",
-  73: "Neige modérée",
-  75: "Neige forte",
-  77: "Grains de neige",
-  80: "Averses légères",
-  81: "Averses modérées",
-  82: "Averses violentes",
-  85: "Averses de neige légères",
-  86: "Averses de neige fortes",
-  95: "Orage",
-  96: "Orage avec grêle légère",
-  99: "Orage avec grêle forte",
+const weatherIcons = {
+  0: "☀️",
+  1: "🌤️",
+  2: "⛅",
+  3: "☁️",
+  45: "🌫️",
+  48: "🌫️",
+  51: "🌦️",
+  53: "🌦️",
+  55: "🌧️",
+  56: "🌧️",
+  57: "🌧️",
+  61: "🌧️",
+  63: "🌧️",
+  65: "🌧️",
+  66: "🌨️",
+  67: "🌨️",
+  71: "❄️",
+  73: "❄️",
+  75: "❄️",
+  77: "🌨️",
+  80: "🌦️",
+  81: "🌧️",
+  82: "⛈️",
+  85: "🌨️",
+  86: "🌨️",
+  95: "⛈️",
+  96: "⛈️",
+  99: "⛈️",
 };
 
 // =========================================== API ===============================================
@@ -68,7 +68,12 @@ const renderOpenMeteo = async (city) => {
   // TODO
   const data = await callOpenMeteoAPI(cities[city].lat, cities[city].lon);
   const currentWeather = document.querySelector(".current-weather");
-  currentWeather.textContent = `${weatherDescriptions[data.current.weather_code]} ${data.current.temperature_2m}°C`;
+  currentWeather.textContent = `${weatherIcons[data.current.weather_code]} ${data.current.temperature_2m}°C`;
+  const listItems = document.querySelectorAll("li");
+  data.daily.time.forEach((day, index) => {
+    listItems[index].textContent =
+      `${new Date(day).toLocaleDateString("fr-FR", { weekday: "short", day: "numeric", month: "short", year: "numeric" })} : ${weatherIcons[data.daily.weather_code[index]]} ${data.daily.temperature_2m_min[index]} / ${data.daily.temperature_2m_max[index]}°C`;
+  });
 };
 
 // =========================================== DOM ===============================================
